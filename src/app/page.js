2166886c0d96cc4c528b37072c59c8da41635e5a
Bharath <kamepallilakshmanbharathk4@gmail.com>
2024-01-12 12:43:13 +0530

    branch-1

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className="text-[32px] text-center font-bold">Hello World!</div>;
+  return (
+    <div className="text-[32px] text-center font-bold">
+      Testing from branch-1
+    </div>
+  );
 }
